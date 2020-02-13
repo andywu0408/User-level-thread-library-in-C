1ad505acdd6ecf06af82c6d501f69e25b08c9dd5
zsxing99 <zsxing99@gmail.com>
2020-02-12 23:57:16 -0800

    -phase 1 raw implementation -untested

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -6,31 +6,91 @@
 #include "thread.h"
 
 struct semaphore {
-	/* TODO Phase 1 */
+    /* the preserved data count */
+	size_t count;
+	/* the queue of blocked threads */
+	queue_t waiting_list;
 };
 
 sem_t sem_create(size_t count)
 {
-	/* TODO Phase 1 */
+	sem_t curr = (sem_t)malloc(sizeof(struct semaphore));
+	if (curr == NULL) {
+        return NULL;
+	}
+	curr->count = count;
+	curr->waiting_list = queue_create();
+	if (curr->waiting_list == NULL) {
+        return NULL;
+	}
+    return curr;
 }
 
 int sem_destroy(sem_t sem)
 {
-	/* TODO Phase 1 */
+	if (sem == NULL || queue_length(sem->waiting_list) <= 0) {
+        return -1;
+	}
+
+	enter_critical_section();
+	queue_destroy(sem->waiting_list);
+	free(sem);
+	exit_critical_section();
+    return 0;
 }
 
 int sem_down(sem_t sem)
 {
-	/* TODO Phase 1 */
+	if (sem == NULL) {
+        return -1;
+	}
+    enter_critical_section();
+	if (sem->count == 0) {
+	    /* nothing in the sem */
+        int* tid = malloc(sizeof(int));
+        *tid = pthread_self();
+        queue_enqueue(sem->waiting_list, tid);
+
+        /* start waiting in the queue */
+        thread_block();
+
+        /* back */
+        free(tid)
+	}
+
+	sem->count--;
+	exit_critical_section();
+    return 0;
 }
 
 int sem_up(sem_t sem)
 {
-	/* TODO Phase 1 */
+	if (sem == NULL) {
+        return -1;
+	}
+	enter_critical_section();
+	sem->count++;
+
+	if (queue_length(sem->waiting_list) >= 1) {
+        void** data = malloc(sizeof(void*));
+        queue_dequeue(sem->waiting_list, data);
+        pthread_t* next = (pthread_t)(*data);
+        thread_unblock(*next);
+	}
+
+	exit_critical_section();
+    return 0;
 }
 
 int sem_getvalue(sem_t sem, int *sval)
 {
-	/* TODO Phase 1 */
+	if (sem == NULL || sval == NULL) {
+        return -1;
+	}
+
+	enter_critical_section();
+	*sval = sem->count == 0 ? -queue_length(sem->waiting_list) : sem->count;
+	exit_critical_section();
+    return 0;
 }
 
