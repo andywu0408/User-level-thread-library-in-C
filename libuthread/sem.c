8c295eba2c5b3293e854decd6ab1bdd425f98b18
zsxing99 <zsxing99@gmail.com>
2020-02-13 20:38:01 -0800

    -fix indentation in sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -7,46 +7,46 @@
 
 struct semaphore {
     /* the preserved data count */
-	size_t count;
-	/* the queue of blocked threads */
-	queue_t waiting_list;
+    size_t count;
+    /* the queue of blocked threads */
+    queue_t waiting_list;
 };
 
 sem_t sem_create(size_t count)
 {
-	sem_t curr = (sem_t)malloc(sizeof(struct semaphore));
-	if (curr == NULL) {
+    sem_t curr = (sem_t)malloc(sizeof(struct semaphore));
+    if (curr == NULL) {
         return NULL;
-	}
-	curr->count = count;
-	curr->waiting_list = queue_create();
-	if (curr->waiting_list == NULL) {
+    }
+    curr->count = count;
+    curr->waiting_list = queue_create();
+    if (curr->waiting_list == NULL) {
         return NULL;
-	}
+    }
     return curr;
 }
 
 int sem_destroy(sem_t sem)
 {
-	if (sem == NULL || queue_length(sem->waiting_list) <= 0) {
+    if (sem == NULL || queue_length(sem->waiting_list) <= 0) {
         return -1;
-	}
+    }
 
-	enter_critical_section();
-	queue_destroy(sem->waiting_list);
-	free(sem);
-	exit_critical_section();
+    enter_critical_section();
+    queue_destroy(sem->waiting_list);
+    free(sem);
+    exit_critical_section();
     return 0;
 }
 
 int sem_down(sem_t sem)
 {
-	if (sem == NULL) {
+    if (sem == NULL) {
         return -1;
-	}
+    }
     enter_critical_section();
     while (sem->count == 0) {
-	    /* nothing in the sem */
+        /* nothing in the sem */
         pthread_t* tid = malloc(sizeof(pthread_t));
         *tid = pthread_self();
         queue_enqueue(sem->waiting_list, tid);
@@ -56,7 +56,7 @@ int sem_down(sem_t sem)
 
         /* back */
         free(tid);
-	}
+    }
 
 	sem->count--;
 	exit_critical_section();
@@ -65,32 +65,32 @@ int sem_down(sem_t sem)
 
 int sem_up(sem_t sem)
 {
-	if (sem == NULL) {
+    if (sem == NULL) {
         return -1;
-	}
-	enter_critical_section();
-	sem->count++;
+    }
+    enter_critical_section();
+    sem->count++;
 
-	if (queue_length(sem->waiting_list) >= 1) {
+    if (queue_length(sem->waiting_list) >= 1) {
         void** data = malloc(sizeof(void*));
         queue_dequeue(sem->waiting_list, data);
         pthread_t* next = (pthread_t*)(*data);
         thread_unblock(*next);
-	}
+    }
 
-	exit_critical_section();
+    exit_critical_section();
     return 0;
 }
 
 int sem_getvalue(sem_t sem, int *sval)
 {
-	if (sem == NULL || sval == NULL) {
+    if (sem == NULL || sval == NULL) {
         return -1;
-	}
+    }
 
-	enter_critical_section();
-	*sval = sem->count == 0 ? -queue_length(sem->waiting_list) : sem->count;
-	exit_critical_section();
+    enter_critical_section();
+    *sval = sem->count == 0 ? -queue_length(sem->waiting_list) : sem->count;
+    exit_critical_section();
     return 0;
 }
 
